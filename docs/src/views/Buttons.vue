dcda5188dd2b92a542f937f1638304f78c5a53a5
Peter Morrison <bwt.bluewebtech@gmail.com>
2020-05-19 08:18:50 -0400

    Working on buttons and feeling out the initial foundation.

---
+++ docs/src/views/Buttons.vue
@@ -0,0 +1,96 @@
+<template>
+  <div align="center">
+    <div style="padding:10px;">
+      <button class="button xsmall">xsmall</button>
+      <button class="button small">small</button>
+      <button class="button">default</button>
+      <button class="button medium">medium</button>
+      <button class="button large">large</button>
+      <button class="button xlarge">xlarge</button>
+      <button class="button rounded">large</button>
+      <button class="button shadow">large</button>
+      <button class="button border">large</button>
+    </div>
+    <div style="padding:10px;">
+      <button class="button primary xsmall">Primary</button>
+      <button class="button primary small">Primary</button>
+      <button class="button primary">Primary</button>
+      <button class="button primary medium">Primary</button>
+      <button class="button primary large">Primary</button>
+      <button class="button primary xlarge">Primary</button>
+      <button class="button primary rounded">Primary</button>
+      <button class="button primary shadow">Primary</button>
+      <button class="button primary border">Primary</button>
+    </div>
+    <div style="padding:10px;">
+      <button class="button secondary xsmall">Secondary</button>
+      <button class="button secondary small">Secondary</button>
+      <button class="button secondary">Secondary</button>
+      <button class="button secondary medium">Secondary</button>
+      <button class="button secondary large">Secondary</button>
+      <button class="button secondary xlarge">Secondary</button>
+    </div>
+    <div style="padding:10px;">
+      <button class="button success xsmall">Success</button>
+      <button class="button success small">Success</button>
+      <button class="button success">Success</button>
+      <button class="button success medium">Success</button>
+      <button class="button success large">Success</button>
+      <button class="button success xlarge">Success</button>
+    </div>
+    <div style="padding:10px;">
+      <button class="button danger xsmall">Danger</button>
+      <button class="button danger small">Danger</button>
+      <button class="button danger">Danger</button>
+      <button class="button danger medium">Danger</button>
+      <button class="button danger large">Danger</button>
+      <button class="button danger xlarge">Danger</button>
+    </div>
+    <div style="padding:10px;">
+      <button class="button warning xsmall">Warning</button>
+      <button class="button warning small">Warning</button>
+      <button class="button warning">Warning</button>
+      <button class="button warning medium">Warning</button>
+      <button class="button warning large">Warning</button>
+      <button class="button warning xlarge">Warning</button>
+    </div>
+    <div style="padding:10px;">
+      <button class="button caution xsmall">Caution</button>
+      <button class="button caution small">Caution</button>
+      <button class="button caution">Caution</button>
+      <button class="button caution medium">Caution</button>
+      <button class="button caution large">Caution</button>
+      <button class="button caution xlarge">Caution</button>
+    </div>
+    <div style="padding:10px;">
+      <button class="button notice xsmall">Notice</button>
+      <button class="button notice small">Notice</button>
+      <button class="button notice">Notice</button>
+      <button class="button notice medium">Notice</button>
+      <button class="button notice large">Notice</button>
+      <button class="button notice xlarge">Notice</button>
+    </div>
+    <div style="padding:10px;">
+      <button class="button power xsmall">Power</button>
+      <button class="button power small">Power</button>
+      <button class="button power">Power</button>
+      <button class="button power medium">Power</button>
+      <button class="button power large">Power</button>
+      <button class="button power xlarge">Power</button>
+    </div>
+    <div style="padding:10px;">
+      <button class="button sweet xsmall">Sweet</button>
+      <button class="button sweet small">Sweet</button>
+      <button class="button sweet">Sweet</button>
+      <button class="button sweet medium">Sweet</button>
+      <button class="button sweet large">Sweet</button>
+      <button class="button sweet xlarge">Sweet</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .button {
+    margin-right: 20px;
+  }
+</style>
